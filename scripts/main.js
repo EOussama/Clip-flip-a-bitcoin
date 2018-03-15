8d61eb8300819f873701d04c7a3c04588e45d385
Oussama Essamadi <Oussama_gtacj@live.fr>
2018-03-15 00:26:06 +0000

    Add files via upload
    + Style changes.
    + Score added to the result panel.

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,11 @@ window.addEventListener('load', function() { "use strict";
 	const
 		coin = document.getElementById('coin'),
 		resultPanel = document.getElementById('result'),
+		resultLabel = document.getElementById('label'),
+		resultStats = document.getElementById('stats'),
 		result = ['Tail', 'Head'];
+	
+	var score = { tails : 0, heads : 0 }
 	
 	coin.addEventListener('click', function() {
 		if(this.classList.contains('animate') === false) {
@@ -13,7 +17,13 @@ window.addEventListener('load', function() { "use strict";
 			setTimeout(function() {
 				let rand = Math.floor(Math.random() * 2);
 				
-				resultPanel.textContent = result[rand];
+				if(rand == 0)
+					score.tails++;
+				else
+					score.heads++;
+				
+				resultLabel.textContent = result[rand];
+				resultStats.textContent = `T - ${score.tails} / H - ${score.heads}`;
 				resultPanel.style.top = '0px';
 				resultPanel.style.opacity = '1';
 				coin.classList.remove('animate');
